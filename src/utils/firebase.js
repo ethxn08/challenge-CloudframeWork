b850d7ed161628dc1481eb149f727e3eb982885f
ethxn08 <ethanalfaro08@gmail.com>
2023-05-28 09:57:19 +0200

    Add Firebase Hosting

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-yN7lrWSY5eTMZp-uYBP9CujV6Ytqcgg",
+  authDomain: "challenge-cloudframework.firebaseapp.com",
+  projectId: "challenge-cloudframework",
+  storageBucket: "challenge-cloudframework.appspot.com",
+  messagingSenderId: "345162321682",
+  appId: "1:345162321682:web:03144717a71012c86eadcb",
+};
+
+const app = initializeApp(firebaseConfig);
